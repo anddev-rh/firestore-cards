a92c3a7659b2a0cf770165653fe05bf09c2e432e
anddev-rh <andresrh222@gmail.com>
2021-08-19 20:12:17 -0500

    connect with firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC1iNEVN-o67lrn2VSXGTfo2hab7iPaigo",
+  authDomain: "fb-cards-1b5bc.firebaseapp.com",
+  projectId: "fb-cards-1b5bc",
+  storageBucket: "fb-cards-1b5bc.appspot.com",
+  messagingSenderId: "821375989982",
+  appId: "1:821375989982:web:dd84f9fe680aca43846a4f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
